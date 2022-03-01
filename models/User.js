22a146c0e8b4b3f65f42abe69b31bc7d8bce7534
Zackery Arsement <zarsement@hotmail.com>
2022-02-28 20:23:25 -0600

    Model files/layouts are completed

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+import { isEmail } from 'validator';
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [ isEmail, 'invalid email' ],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema
+.virtual('friendCount')
+.get(function(){
+    return this.friends.length;
+});
+
+
+const User = model('user', userSchema);
+
+module.exports = User;
